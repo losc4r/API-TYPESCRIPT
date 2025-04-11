f95999ec03360a62dee679283655871c59f770f0
CARLOS ANTONIO BASTOS CARVALHO JUNIOR <carlos.abcjunior2@senacsp.edu.br>
2025-04-11 17:09:11 -0300

    db1

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import route from "./routers/userRouter";
+import cors from "cors"
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api",route)
 
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { getUsers} from "../controllers/userController";
-
+import { getUsers, create, update, deleta } from "../controllers/userController";
 const route = Router();
-route.get("/users",getUsers);
-
+route.get("/users", getUsers);
+route.post("/create", create);
+route.put("/update/:id", update)
+route.delete("/delete/:id", deleta)
 
 export default route;
